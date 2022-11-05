25ecc88533a568e3dc7e5cfb6fa1e6f0d8a8d422
AWA DONACIEN <54797997+donalawa@users.noreply.github.com>
2022-11-05 23:17:03 +0100

    Update questionsRouter.js

--- routes/questionsRouter.js
+++ routes/questionsRouter.js
@@ -2,7 +2,5 @@ const express = require('express');
 const questionController = require('../controllers/questionController');
 
 const questionRouter = express.Router();
-
 questionRouter.post('/questions', questionController.addQuestionFb);
-
 module.exports = questionRouter;
